6362aab61db271f74542e592eaa24b0141ec203d
filipe <feliipequintaniilha@gmail.com>
2019-11-22 14:33:10 -0300

    Neste projeto, fiz as duas primeiras funções c++

--- trab.cpp
+++ trab.cpp
@@ -4,12 +4,39 @@ using namespace std;
 /*
 	Conta todas as letras 'letra' contidas em 'texto'
 */
-
-
+int contletras(string texto)
+{	int tam;
+	tam=texto.length();
+	int ql=0; 
+	int i=0;
+	while(i<tam)
+	{
+		if(texto[i]!=' ')
+		{
+		 ql=ql+1;
+		}	
+		i++;
+	}
+	return ql;
+}
 /*
 	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
 */
+string substitui(string texto, char letra , char letra2)
+{
 
+ 	int	tam=texto.length();
+	int i=0;
+	while(i<tam)
+	{
+		if(texto[i]==letra)
+		{
+		texto[i]=letra2;
+		}
+		i++;	
+	}
+	return texto;
+}
 
 /*
 	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
@@ -44,7 +71,7 @@ string substituirPalavras(string palavra1, string palavra2, string texto)
 main(){
 string txt;
 char let, let2;
-string pal, pal2, tx2;
+string pal, pal2, tx2,tx1,tx3;
 
 	cout << "Entre com o texto para fazer o teste:";
 	getline(cin, txt);
@@ -56,12 +83,18 @@ string pal, pal2, tx2;
 	cin>>pal;
 	cout<<"\n Palavra a substituir: ";
 	cin>>pal2;
-	
+	tx1=txt;
+	int qnl;
+	qnl=contletras(tx1);
+	cout<<"\n A quantidade de letras eh: "<<qnl;
+	tx3=substitui(tx1, let, let2);
+	cout<<"\n O texto com a letra trocada eh: "<<tx3;
 	int qnt2;
 	qnt2 = contaPalavras(pal, txt);
 	cout<<"\n A quantidade da repeticao da plavra '"<<pal<<"' eh: "<<qnt2;
 	tx2=substituirPalavras(pal, pal2, txt);
 	cout<<"\n O novo texto eh: "<<tx2;
-
+	
+	
 }
 
